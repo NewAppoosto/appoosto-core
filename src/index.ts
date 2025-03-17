5887257652a827739707317ee3ebb9ad27f00ae3
Akash <mahmudakash177@gmail.com>
2025-03-17 15:58:37 +0600

    fix bugs

--- src/index.ts
+++ src/index.ts
@@ -9,23 +9,6 @@ export * from "./types";
 
 // Base interface
 export * from "./interface/base.interface";
+export * from "./graphql";
+export * from "./interface";
 
-// User microservice
-export * from "./user/interfaces";
-export * from "./user/types.graphql";
-
-// Profile microservice
-export * from "./profile/interfaces";
-export * from "./profile/types.graphql";
-
-// Authorization microservice
-export * from "./authorization/interfaces";
-export * from "./authorization/types.graphql";
-
-// Notification microservice
-export * from "./notification/interfaces";
-export * from "./notification/types.graphql";
-
-// Media microservice
-export * from "./media/interfaces";
-export * from "./media/types.graphql";
