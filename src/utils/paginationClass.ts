7eddcfcaa9e7f87772a30001519c10370c1be376
Akash <mahmudakash177@gmail.com>
2025-03-18 10:39:30 +0600

    Add pagination generation class to unique name for every entity

--- src/utils/paginationClass.ts
+++ src/utils/paginationClass.ts
@@ -2,7 +2,9 @@ import { Field, ObjectType } from "@nestjs/graphql";
 import { PaginationBase } from "../graphql";
 
 export function PaginatedResponse<TItem>(TItemClass: new () => TItem) {
-  @ObjectType({ isAbstract: true })
+  // Get the name of the item class to create a unique name for the paginated class
+  const className = TItemClass.name;
+  @ObjectType(`Paginated${className}`)
   abstract class PaginatedResponseClass extends PaginationBase {
     @Field(() => [TItemClass])
     nodes!: TItem[];
