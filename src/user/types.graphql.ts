4ae16083dcccd7361c8c5420d55b91bdb23c3bc9
Akash <mahmudakash177@gmail.com>
2025-04-15 14:00:54 +0600

    use shareable type

--- src/user/types.graphql.ts
+++ src/user/types.graphql.ts
@@ -22,59 +22,48 @@ registerEnumType(InvitationStatus, {
   description: "Status of an invitation",
 });
 
-@ObjectType({ description: "Company details", isAbstract: true })
+@ObjectType({ description: "Company details" })
 @Directive('@key(fields: "id")')
-@Directive("@extends")
+@Directive("@shareable")
 export class CompanyDetails {
   @Field(() => ID)
-  @Directive("@external")
   id!: string;
 
   @Field({ description: "Company name" })
-  @Directive("@external")
   name!: string;
 
   @Field({ description: "Legal address of the company" })
-  @Directive("@external")
   legal_address!: string;
 
   @Field({ description: "VAT number of the company" })
-  @Directive("@external")
   vat_number!: string;
 }
 
 @ObjectType({ description: "User details" })
 @Directive('@key(fields: "id")')
-@Directive("@extends")
+@Directive("@shareable")
 export class UserDetails implements IUserDetails {
   @Field(() => ID)
-  @Directive("@external")
   id!: string;
 
   @Field({
     description:
       "Username of the user. Unique username for every user is required",
   })
-  @Directive("@external")
   username!: string;
 
   @Field({ description: "Email of the user. It have to be unique" })
-  @Directive("@external")
   email!: string;
 
   @Field({ defaultValue: false })
-  @Directive("@external")
   is_google_connected!: boolean;
 
   @Field({ defaultValue: false })
-  @Directive("@external")
   is_github_connected!: boolean;
 
   @Field({ defaultValue: false })
-  @Directive("@external")
   is_linked_in_connected!: boolean;
 
   @Field({ defaultValue: false })
-  @Directive("@external")
   twofa_enabled!: boolean;
 }
