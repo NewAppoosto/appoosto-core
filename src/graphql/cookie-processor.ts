3c544c876a3bf2585fbe8228a6c3949e408e3873
Akash <mahmudakash177@gmail.com>
2025-05-08 13:42:32 +0600

    cookie processor

---
+++ src/graphql/cookie-processor.ts
@@ -0,0 +1,50 @@
+import { RemoteGraphQLDataSource } from '@apollo/gateway';
+import {
+  ApolloServerPlugin,
+  GraphQLRequestContext,
+  GraphQLRequestListener,
+} from '@apollo/server';
+
+interface ServerContext {
+  passthrough_cookies?: string;
+  [key: string]: any;
+}
+
+export class CookieProcessorDataSource extends RemoteGraphQLDataSource {
+  didReceiveResponse({
+    response,
+    context,
+  }: {
+    response: any;
+    context: any;
+  }): any {
+    if (response.http?.headers && response.http.headers.get('set-cookie')) {
+      const setCookieHeader = response.http.headers.get('set-cookie');
+      if (setCookieHeader) {
+        context.passthrough_cookies = setCookieHeader;
+      }
+    }
+    return response;
+  }
+}
+
+export class CookieServerListener
+  implements GraphQLRequestListener<ServerContext>
+{
+  public willSendResponse({
+    contextValue,
+    response,
+  }: GraphQLRequestContext<ServerContext>): Promise<void> {
+    if (contextValue?.passthrough_cookies) {
+      response.http.headers.set('set-cookie', contextValue.passthrough_cookies);
+    }
+
+    return Promise.resolve();
+  }
+}
+
+export class CookieServerPlugin implements ApolloServerPlugin<ServerContext> {
+  async requestDidStart(): Promise<GraphQLRequestListener<ServerContext> | void> {
+    return new CookieServerListener();
+  }
+}
--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,3 +2,4 @@ export * from "../user/types.graphql";
 export * from "../common/types.graphql";
 export * from "./common";
 export * from "./scalar";
+export * from "./cookie-processor";
